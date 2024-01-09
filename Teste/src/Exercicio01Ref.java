c9eebad16663b406d2a1eae7ec74b258cfa82fe2
gabriel <gabriel.pedreiro@hotmail.com>
2024-01-08 21:17:51 -0300

    Refatorando Exercício 01.

---
+++ Teste/src/Exercicio01Ref.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Exercicio01Ref {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        List<String> nomes = new ArrayList<>();
+        List<Integer> idades = new ArrayList<>();
+        List<String> idadesCrescente = new ArrayList<>();
+
+
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Informe um nome: ");
+            nome = ler.nextLine();
+            nomes.add(nome);
+            System.out.println("Informe a idade: ");
+            idade = ler.nextInt();
+            idades.add(idade);
+            ler.nextLine();
+        }
+
+
+
+        System.out.println("Resultado: ");
+        System.out.println("Nomes: " + nomes);
+        System.out.println("Idades: " + idades);
+        Collections.sort(idades);
+        System.out.println(("Idades apresentadas em ordem crescente: " + idades));
+
+
+    }
+}
---
+++ Teste/src/Exercicio02Ref.java
@@ -0,0 +1,5 @@
+public class Exercicio02Ref {
+    public static void main(String[] args) {
+
+    }
+}
