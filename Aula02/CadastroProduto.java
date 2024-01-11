5734031631745aea042a4a51c61e1eb27aeb6aba
gabriel <gabriel.pedreiro@hotmail.com>
2024-01-10 21:57:15 -0300

    Criar LitagemDeProdutos

--- Aula02/CadastroProduto.java
+++ Aula02/CadastroProduto.java
@@ -1,79 +1,117 @@
 import Produto.Produto;
+import Produto.ListagemDeProdutos;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 public class CadastroProduto {
-    /*
-    - Visualizar produtos;
-    - Cadastrar produtos;
-    - Ativa/Desativar produtos;
-    - Adicionar estoque;
-    - Retirar estoque;
-    - Tudo deve funcionar dentro de um menu, sem encerrar o programa.
-     */
 
     public static void main(String[] args) {
-        // CRIAR LISTA DO TIPO PRODUTO
-        ArrayList<Produto> listaDeProdutos = new ArrayList<>();
         Scanner read = new Scanner(System.in);
+        ArrayList<Produto> listaProdutos = new ArrayList<>();
         int choice = 0;
+        int contador = 0;
 
-        while (choice != 5) {
-
+        while (choice != 6) {
             System.out.println("---------------------------");
             System.out.println("01 - Visualizar produtos.");
             System.out.println("02 - Cadastrar produto.");
             System.out.println("03 - Ativar / Inativar produto.");
             System.out.println("04 - Estoque.");
-            System.out.println("05 - Sair.");
+            System.out.println("05 - Listagem de produtos.");
+            System.out.println("06 - Sair.");
             System.out.println(("-> Selecione uma opção: "));
             choice = read.nextInt();
 
             switch (choice) {
                 case 1:
-                    for (int i = 0; i < listaDeProdutos.size(); i++) {
+                    for (int i = 0; i < listaProdutos.size(); i++) {
                         System.out.println("---------------------------");
-                        System.out.println("Nome: " + listaDeProdutos.get(i).getNomeProduto());
-                        System.out.println("Status: " + listaDeProdutos.get(i).getStatusProduto());
-                        System.out.println("Estoque: " + listaDeProdutos.get(i).getEstoqueAtualProduto());
-
+                        System.out.println("ID: " + listaProdutos.get(i).getIdProduto());
+                        System.out.println("Nome: " + listaProdutos.get(i).getNomeProduto());
+                        System.out.println("Estoque: " + listaProdutos.get(i).getEstoqueProduto());
+                        System.out.println("Status: " + listaProdutos.get(i).getStatusProduto());
                     }
                     break;
 
                 case 2:
-                    System.out.println("Informe o nome do produto: ");
+                    System.out.println("- Digite o nome do produto a ser cadastrado: ");
                     String nome = read.next();
-                    System.out.println("Informe o estoque: ");
+                    System.out.println("- Digite a quantidade em estoque do produto " + nome + ":");
                     int estoque = read.nextInt();
-                    listaDeProdutos.add(new Produto(nome, estoque));
-                    //System.out.print(String.format("\033[H\033[2J"));
+                    contador ++;
+                    listaProdutos.add(new Produto(nome, estoque, contador));
                     break;
 
                 case 3:
-                    System.out.println("De qual produto você deseja alterar o status? ");
-                    String nomeAlteraStatus = read.next();
-                    for (int i = 0; i < listaDeProdutos.size(); i++) {
-                        if (listaDeProdutos.get(i).getNomeProduto().equals(nomeAlteraStatus)) {
-                            System.out.println("Insira o novo status do produto " + nomeAlteraStatus + ": ATIVO | INATIVO.");
-                            String status = read.next();
-                            if (status.equals("ativo")){
-                                listaDeProdutos.get(i).setStatusProduto(true);
+                    System.out.println("- Informe o ID do produto: ");
+                    int idStatus = read.nextInt();
+                    for (int i = 0; i < listaProdutos.size(); i++) {
+                        if (listaProdutos.get(i).getIdProduto() == idStatus) {
+                            System.out.println("- Informe o novo status: ATIVO | INATIVO");
+                            //TESTAR ISSO AQUI
+                            String novoStatusProduto = read.next().toLowerCase(Locale.ROOT);
+                            if (novoStatusProduto.equals("ativo")) {
+                                listaProdutos.get(i).setStatusProduto(true);
+                                System.out.println("Status alterado com sucesso!");
+                            } else if (novoStatusProduto.equals("inativo")) {
+                                listaProdutos.get(i).setStatusProduto(false);
+                                System.out.println("Status alterado com sucesso!");
                             } else {
-                                listaDeProdutos.get(i).setStatusProduto(false);
+                                System.out.println("O status informado é inválido!");
                             }
                         }
                     }
                     break;
-                case 4:
 
+                case 4:
+                    System.out.println("- Informe o ID do produto: ");
+                    int idEstoque = read.nextInt();
+                    for (int i = 0; i < listaProdutos.size(); i++) {
+                        if (listaProdutos.get(i).getIdProduto() == idEstoque) {
+                            System.out.println("- Informe a operação: ADICIONAR | RETIRAR");
+                            String operacao = read.next().toLowerCase();
+                            if (operacao.equals("adicionar")) {
+                                int novoEstoqueMaior = 0;
+                                while (novoEstoqueMaior <= 0) {
+                                    System.out.println("- Digite a quantidade a ser adicionada: ");
+                                    novoEstoqueMaior = read.nextInt();
+                                    if (novoEstoqueMaior > 0) {
+                                        novoEstoqueMaior += listaProdutos.get(i).getEstoqueProduto();
+                                        listaProdutos.get(i).setEstoqueProduto(novoEstoqueMaior);
+                                        System.out.println("Estoque alterado com sucesso!");
+                                    } else {
+                                        System.out.println("O valor informado deve ser maior que 0!");
+                                    }
+                                }
+                            } else if (operacao.equals("retirar")) {
+                                int valorTransitorioEstoqueMenor = 0;
+                                while (valorTransitorioEstoqueMenor <= 0) {
+                                    System.out.println("- Digite a quantidade a ser retirada: ");
+                                    valorTransitorioEstoqueMenor = read.nextInt();
+                                    if (valorTransitorioEstoqueMenor > 0) {
+                                        int novoEstoqueMenor = listaProdutos.get(i).getEstoqueProduto();
+                                        novoEstoqueMenor -= valorTransitorioEstoqueMenor;
+                                        listaProdutos.get(i).setEstoqueProduto(novoEstoqueMenor);
+                                        System.out.println("Estoque alterado com sucesso!");
+                                    } else {
+                                        System.out.println("O valor informado deve ser maior que 0!");
+                                    }
+                                }
+                            }
+                        }
+                    }
                     break;
+
                 case 5:
+                    System.out.println(ListagemDeProdutos.imprimirMenuDeListagens());
+                    break;
+
+                case 6:
+                    System.out.println("Fim do programa :)");
                     break;
             }
-
         }
-
-        System.out.println("Fim do programa.");
 
     }
 }
--- Aula02/Produto/Produto.java
+++ Aula02/Produto/Produto.java
@@ -1,40 +1,59 @@
 package Produto;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Produto {
     private String nomeProduto;
     private boolean statusProduto;
-    private int estoqueAtualProduto;
+    private int estoqueProduto;
 
-    public Produto(String nomeProduto, int estoqueAtualProduto) {
+    private int idProduto;
+
+    public Produto(String nomeProduto, int estoqueProduto, int idProduto) {
         this.nomeProduto = nomeProduto;
         this.statusProduto = true;
-        this.estoqueAtualProduto = estoqueAtualProduto;
+        this.estoqueProduto = estoqueProduto;
+        this.idProduto = idProduto;
     }
 
-    // SET NOME
-    public void setNomeProduto(String nome) {
-        this.nomeProduto = nome;
-    }
+    Scanner read = new Scanner(System.in);
+
+    public Produto() {} // Construtor default chamado em ListagemDeProdutos.
 
     public String getNomeProduto() {
         return nomeProduto;
     }
 
-    // SET ESTOQUE
-    public void setEstoqueAtualProduto(int valor) {
-        this.estoqueAtualProduto = valor;
-    }
-
-    public int getEstoqueAtualProduto() {
-        return estoqueAtualProduto;
-    }
-
-    // SET STATUS
-    public void setStatusProduto(boolean status) {
-        this.statusProduto = status;
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
     }
 
     public boolean getStatusProduto() {
         return statusProduto;
+    }
+
+    public void setStatusProduto(boolean statusProduto) {
+        this.statusProduto = statusProduto;
+    }
+
+    public int getEstoqueProduto() {
+        return estoqueProduto;
+    }
+
+    public void setEstoqueProduto(int estoqueProduto) {
+        this.estoqueProduto = estoqueProduto;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public int contadorId() {
+        return this.idProduto = idProduto++;
     }
 }
