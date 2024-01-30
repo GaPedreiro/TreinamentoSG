5dd7bac893d3027dde9217cf3cca99270902fadc
gabriel <gabriel.pedreiro@hotmail.com>
2024-01-29 21:55:53 -0300

    Desenvolve Log repository

--- Lanchonete/src/main/java/com/Lanchonete/LOG/Log.java
+++ Lanchonete/src/main/java/com/Lanchonete/LOG/Log.java
@@ -17,4 +17,19 @@ public class Log {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Column(name = "saldo")
+    private double saldoCliente;
+
+    public Log(double saldoCliente) {
+        this.saldoCliente = saldoCliente;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public double getSaldoCliente() {
+        return saldoCliente;
+    }
 }
--- Lanchonete/src/main/java/com/Lanchonete/LanchoneteApplication.java
+++ Lanchonete/src/main/java/com/Lanchonete/LanchoneteApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LanchoneteApplication {
 
+	// NECESSÁRIO CRIAR UMA TABELA DE RECEITAS, PARA PODER QUANTIFICAR CADA INGREDIENTE DE UMA RECEITA.
+
 	public static void main(String[] args) {
 		SpringApplication.run(LanchoneteApplication.class, args);
 	}
